6d1d97dc23f0690846ceaf064ad3057ebaa00a70
darkarr666 <darkarr666@outlook.com>
2025-07-18 00:48:06 -0600

    se agregó ejercicio 9, y se modificó 7, ejercicio 9 falta corregir (quitar try except)

--- ejercicio7.py
+++ ejercicio7.py
@@ -1,7 +1,7 @@
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 15, 16, 17] # Definimos listas
 pares = []
-for numero in lista:
-    if numero % 2 == 0:
+for numero in lista: # Recorremos la lista principal
+    if numero % 2 == 0: # Si el modulo de la división de cada numero entre 2, agrega el numero a la liasta vacía 
         pares.append(numero)
-print("Lista de pares: ", ", ".join(str(numero)for numero in pares))
+print("Lista de pares: ", ", ".join(str(numero)for numero in pares)) # Imprimimos la lista de pares
 
---
+++ ejercicio9.py
@@ -0,0 +1,46 @@
+#este programa pide palabras hasta que se ingrese "fin", para luego separarlas en 3 listas diferentes dependiendo de su longitud
+
+def pedirpalabras():
+    try:
+        palabra = input("Ingresa una palabra: ")
+        if palabra.lower() == "fin":
+            return "fin"
+        else:
+            if len(palabra) >= 1:
+                return palabra
+            else:
+                print("No puedes ingresar cadenas vacías")
+                return None
+
+    except ValueError:
+        print("Eso no es una cadena")
+        return None
+
+    
+def imprimirlistas(lista, string): #pasamos la lista y la string (chicas, medianas o grandes)
+    print(f"Se encontraron {len(lista)} palabras {string}: {", ".join(lista)}") #aquí, a diferencia de casos pasados, no se usa el for porque no vamos
+    # a convertir int a string, ya es string entonces solo usamos .join(lista)
+    print("")
+
+cortas = [] #definimos 3 listas vacías para llenarlas 
+medianas = []
+largas = []
+while True:
+    entrada = pedirpalabras()
+    if entrada == None:
+        continue
+    else:
+        if entrada == "fin":
+            break
+    if len(entrada) < 5:
+        cortas.append(entrada)
+    elif 5 <= len(entrada) <= 8:
+        medianas.append(entrada)
+    else: 
+        largas.append(entrada)
+
+print("")
+imprimirlistas(cortas, "cortas") #en este caso, pasamos dos parametros a nuestra función, la lista y una cadena con el tipo, para imprimir conforme cada lista
+imprimirlistas(medianas, "medianas")
+imprimirlistas(largas, "largas")
+
