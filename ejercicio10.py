f91df4a27943248945616617c8c06a83e950a84b
darkarr666 <darkarr666@outlook.com>
2025-07-20 13:04:45 -0600

    Ejercicio 10 terminado, tal vez con errores, falta revisión

--- ejercicio10.py
+++ ejercicio10.py
@@ -11,17 +11,27 @@ def limpiar_texto(texto):
     
 
 def contar_palabras(texto):
-    palabras = texto.split()
-    return palabras
+    palabras = texto.split() # Divide el texto por espacios, o sea, retorna un arreglo que contiene todas las palabras 
+    conteo = {} # definimos nuestro diccionario vacío
+
+    for palabra in palabras:
+        if palabra in conteo: # aquí decimos "si la palabra está en el conteo, le sumamos uno al valor"
+            conteo[palabra] += 1
+        else:
+            conteo[palabra] = 1 # Si no está en el diccionario conteo, la agregamos con el valor 1
+    return conteo
 
 def imprimir_reporte(diccionario):
-    print("")
+    print("Palabras encontradas")
+    for palabra in sorted(diccionario): #sorted ordena alfabeticamente el arreglo o tupla o lo que sea
+        print(f"Palabra {palabra} se encontró -> {diccionario[palabra]} veces ")
    
 
 def main():
     texto = input("Ingresa una frase, luego se contarán cuantas veces aparece cada palabra de esta misma: ")
     nuevo_texto = limpiar_texto(texto)
-    print(contar_palabras(nuevo_texto))
+    conteo = contar_palabras(nuevo_texto)
+    imprimir_reporte(conteo)
     
 
 
