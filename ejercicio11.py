c0ed22426b2e4e6ed873a946e83e258ddae1db9f
darkarr666 <darkarr666@outlook.com>
2025-07-22 02:02:15 -0600

    implementé ejercicio 11

---
+++ ejercicio11.py
@@ -0,0 +1,10 @@
+ditsionario = {"nombre": "aylin", "edad": 21, "estatura": 1.58}
+diccionario2 = {}
+
+
+
+diccionario2["nombre"] = "aylin" #Nombre es la clave, aylin es el valor
+diccionario2["edad"] = 21 #edad es la clave, 21 es el valor
+
+print(diccionario2["nombre"]) #aquí imprimimos el nombre de la aylin
+print(diccionario2["edad"]) #aquí imprimimos la edad de la aylin
